6c71d44d7d54459240a836b63f8f37529dcd9d47
Triin <115346751+CarrotCatT@users.noreply.github.com>
2022-10-22 14:04:17 +0300

    [WIP] - web controller - in progress

---
+++ src/main/java/com/example/endofthegame/controller/SimpleWebController.java
@@ -0,0 +1,19 @@
+package com.example.endofthegame.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class SimpleWebController {
+
+    @GetMapping("/me-web")
+    public String simpleWebWithMyData() {
+        log.info("showing my page :P");
+        return "my-page"; //page name in string (same as in .html file in resoruce templates fold.
+    }
+
+
+
+}
