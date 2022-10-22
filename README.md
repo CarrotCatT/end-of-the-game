9f2e42b2128f5251a6eeef926b90548083525911
Triin <115346751+CarrotCatT@users.noreply.github.com>
2022-10-22 13:21:55 +0300

    [add] - documentation started + some excercises with REST

---
+++ README.md
@@ -0,0 +1,7 @@
+HTTP request and response
+- request
+ - method type: eg. GET, POST, etc. 
+ - url parameters: eg. /me?my-name=Triin&my-surname=P.
+ - headers 
+ - body of the request
+
--- src/main/java/com/example/endofthegame/controller/SimpleController.java
+++ src/main/java/com/example/endofthegame/controller/SimpleController.java
@@ -6,6 +6,7 @@ import com.example.endofthegame.entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,11 +14,15 @@ public class SimpleController {
 
     private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
 
+    // /me?my-name=Triin&my-surname=P.
     @GetMapping("/me")
-    public Person showMyData() {
+    public Person showMyData(@RequestParam(value = "my-name", defaultValue = "Jan") String myName,
+                             @RequestParam (value = "my-surname", defaultValue = "Kowalski") String mySurname) {
         log.info("showing my personal data");
+        log.info("received name: " + myName + ", and surname: " + mySurname); // don't use with logger
+        log.info("received name: [{}], and surname: [{}]", myName, mySurname);
 
-        return new Person("Triin", "Pajanen", 26,
+        return new Person(myName, mySurname, 26,
                 new Address("Estonia", "Laagri"));
     }
 }
